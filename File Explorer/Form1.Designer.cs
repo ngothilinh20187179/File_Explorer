7362f7671ffcdd26ee243605cdbc08f40669cfa2
ngothilinh20187179 <ngothilinh12915@gmail.com>
2021-05-30 11:11:52 +0700

    i love u

---
+++ File Explorer/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Document", 1, 1);
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Download", 2, 2);
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Pictures", 3, 3);
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Videos", 4, 4);
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("This PC", 0, 0, new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.back = new System.Windows.Forms.Button();
+            this.forward = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.path = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.ImageIndex = 1;
+            treeNode1.Name = "Node1";
+            treeNode1.SelectedImageIndex = 1;
+            treeNode1.Text = "Document";
+            treeNode2.ImageIndex = 2;
+            treeNode2.Name = "Node2";
+            treeNode2.SelectedImageIndex = 2;
+            treeNode2.Text = "Download";
+            treeNode3.ImageIndex = 3;
+            treeNode3.Name = "Node3";
+            treeNode3.SelectedImageIndex = 3;
+            treeNode3.Text = "Pictures";
+            treeNode4.ImageIndex = 4;
+            treeNode4.Name = "Node4";
+            treeNode4.SelectedImageIndex = 4;
+            treeNode4.Text = "Videos";
+            treeNode5.ImageIndex = 0;
+            treeNode5.Name = "Node0";
+            treeNode5.SelectedImageIndex = 0;
+            treeNode5.Text = "This PC";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(194, 387);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "This PC.png");
+            this.imageList.Images.SetKeyName(1, "Document.png");
+            this.imageList.Images.SetKeyName(2, "Download.png");
+            this.imageList.Images.SetKeyName(3, "Pictures.png");
+            this.imageList.Images.SetKeyName(4, "Videos.png");
+            this.imageList.Images.SetKeyName(5, "Disk C.png");
+            this.imageList.Images.SetKeyName(6, "");
+            this.imageList.Images.SetKeyName(7, "Network");
+            this.imageList.Images.SetKeyName(8, "Unknown");
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 49);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(753, 387);
+            this.splitContainer1.SplitterDistance = 194;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.Type,
+            this.LastModified});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(555, 387);
+            this.listView1.SmallImageList = this.imageList;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            // 
+            // Name
+            // 
+            this.Name.Text = "Name";
+            this.Name.Width = 154;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Type";
+            this.Type.Width = 173;
+            // 
+            // LastModified
+            // 
+            this.LastModified.Text = "LastModified";
+            this.LastModified.Width = 147;
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(35, 10);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(57, 23);
+            this.back.TabIndex = 2;
+            this.back.Text = "<-";
+            this.back.UseVisualStyleBackColor = true;
+            // 
+            // forward
+            // 
+            this.forward.Location = new System.Drawing.Point(105, 10);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(60, 23);
+            this.forward.TabIndex = 3;
+            this.forward.Text = "->";
+            this.forward.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Your path: ";
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(318, 10);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(447, 22);
+            this.path.TabIndex = 6;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 487);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.forward);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.splitContainer1);
+            //this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader LastModified;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+    }
+}
+
---
+++ File Explorer/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Explorer
+{
+    public partial class Form1 : Form
+    {
+        private int iFiles = 0;
+        private int iDirectories = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void AddDirectories(TreeNode tnSubNode)
+        {
+            treeView1.BeginUpdate();
+            iDirectories = 0;
+
+            try
+            {
+                DirectoryInfo diRoot;
+
+                // Nếu là ổ đĩa thì lấy thư mục từ ổ đỉa
+                if (tnSubNode.SelectedImageIndex < 11)
+                {
+                    diRoot = new DirectoryInfo(tnSubNode.FullPath + "\\");
+                }
+                //  Ngược lại lấy thư mục từ thư mục
+                else
+                {
+                    diRoot = new DirectoryInfo(tnSubNode.FullPath);
+                }
+                DirectoryInfo[] dirs = diRoot.GetDirectories();
+
+                tnSubNode.Nodes.Clear();
+
+                // Add thư mục con vào tree
+                foreach (DirectoryInfo dir in dirs)
+                {
+                    iDirectories++;
+                    TreeNode subNode = new TreeNode(dir.Name);
+                    subNode.ImageIndex = 11;
+                    subNode.SelectedImageIndex = 12;
+                    tnSubNode.Nodes.Add(subNode);
+                }
+
+            }
+            catch {; }
+
+            treeView1.EndUpdate();
+        }
+        private void AddFiles(string strPath)
+        {
+            listView1.BeginUpdate();
+
+            listView1.Items.Clear();
+            iFiles = 0;
+            DirectoryInfo di = new DirectoryInfo(strPath + "\\");
+            FileInfo[] theFiles = di.GetFiles();
+            string _Size = string.Empty;
+            foreach (FileInfo theFile in theFiles)
+            {
+                iFiles++;
+                if (theFile.Length >= 1024)
+                    _Size = string.Format("{0:### ### ###} KB", theFile.Length / 1024);
+                else _Size = string.Format("{0} Bytes", theFile.Length);
+
+                ListViewItem lvItem = new ListViewItem(theFile.Name);
+                lvItem.SubItems.Add(_Size);
+                lvItem.SubItems.Add(theFile.LastWriteTime.ToString("dd/MM/yyyy"));
+                lvItem.SubItems.Add(theFile.LastWriteTime.ToShortTimeString());
+                listView1.Items.Add(lvItem);
+            }
+
+            listView1.EndUpdate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = Environment.GetLogicalDrives(); // liet ke danh sach o dia
+            treeView1.BeginUpdate();
+            foreach (string strDrive in drives)
+            {
+                DriveInfo di = new DriveInfo(strDrive);
+                int driveImage;
+                switch (di.DriveType)
+                {
+                    case DriveType.CDRom:
+                        driveImage = 5;
+                        break;
+
+                    case DriveType.Network:
+                        driveImage = 7;
+                        break;
+
+                    case DriveType.Unknown:
+                        driveImage = 8;
+                        break;
+
+                    default:
+                        driveImage = 6;
+                        break;
+                }
+                TreeNode node = new TreeNode(strDrive.Substring(0, 1), driveImage, driveImage);
+                node.Tag = strDrive;
+
+                if (di.IsReady == true)
+                    node.Nodes.Add("...");
+
+                treeView1.Nodes.Add(node);
+            }
+
+            //for (int i = 0; i < drives.Length; i++) 
+            //{
+            //    treeView1.Nodes[0].Nodes.Add(drives[i]);
+            //}
+            treeView1.ExpandAll();
+            //treeView1.MouseDoubleClick += new MouseEventHandler(treeView1_MouseDoubleClick);
+            treeView1.EndUpdate();
+        }
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes.Count > 0)
+            {
+                if (e.Node.Nodes[0].Text == "..." && e.Node.Nodes[0].Tag == null)
+                {
+                    e.Node.Nodes.Clear();
+
+                    //Liệt kê danh sách thư mục con
+                    string[] dirs = Directory.GetDirectories(e.Node.Tag.ToString());
+
+                    foreach (string dir in dirs)
+                    {
+                        DirectoryInfo di = new DirectoryInfo(dir);
+                        TreeNode node = new TreeNode(di.Name, 0, 1);
+
+                        try
+                        {
+                            //Lưu đường dẫn vào thuộc tính tag của node để sau này sử dụng
+                            node.Tag = dir;
+
+                            //Kiểm tra xem có thư mục con hay không thì thêm 3 dấu chấm
+                            if (di.GetDirectories().Count() > 0)
+                                node.Nodes.Add(null, "...", 0, 0);
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            //Hiển thị Icon thư mục đã bị khóa
+                            node.ImageIndex = 1;
+                            node.SelectedImageIndex = 1;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "DirectoryLister",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            e.Node.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // khi chọn 1 node trên tree thì set path vào textbox(path)
+            path.Text = treeView1.SelectedNode.FullPath;
+
+            // Thêm thư mục con từ ổ đĩa vào tree
+            AddDirectories(e.Node);
+
+            // Thêm file vào listview
+            AddFiles(e.Node.FullPath.ToString());
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Get the selected node
+            string fileName = treeView1.SelectedNode.FullPath;
+          
+        }
+        private void listView1_ItemActivate(object sender, System.EventArgs e)
+        {
+            try
+            {
+                string sPath = treeView1.SelectedNode.FullPath;
+                string sFileName = listView1.FocusedItem.Text;
+
+                Process.Start(sPath + "\\" + sFileName);
+            }
+            catch (Exception Exc) { MessageBox.Show(Exc.ToString()); }
+        }
+    }
+}
